46cfdfac0c27f91b2c41c627caac3630743ac875
akbarmaulino <maulinoakbar8@gmail.com>
2025-08-07 20:18:06 +0700

    Test

--- HelloWord.java
+++ HelloWord.java
@@ -2,6 +2,7 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to Java programming.");
+        System.out.println("This is a simple Java application.");
     }
     
 }
